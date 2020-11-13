5cd1a77769be39dd062aad738f75f063d61d349a
MoPaMo <mpm1301@web.de>
2020-11-13 07:59:11 +0100

    updating data-clicked

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
 $(function () {
-    audioEnabled=true;
-    player = true;
-    $("#selectBar .col").click(function () {
-      id = $(this).index() + 1;
-      notbroken = true;
-      for (var i = 6; i > 1; i--) {
-        elem = $(".row:nth-child(" + i + ") .col:nth-child(" + id + ")");
-        if (elem.attr("data-clicked") != "true") {
-          elem.html((player) ? 'a' : 'b');
-          elem.attr("data-clicked", "true");
-          notbroken = false;
-          break;
-        }
-
+  audioEnabled = true;
+  player = true;
+  function gBP(row, col) {
+    //getElementByPosition
+    return $(".row:nth-child(" + col + ") .col:nth-child(" + row + ")");
+  }
+  function checkVictory(){
+    for(var i=1;i<=8;i++){
+      console.log(gBP(2,i+1))
+      if(gBP(2,i+1).attr("data-clicked")=="a"){
+        console.log("a=a")
       }
-      if (!notbroken) {
-        if(audioEnabled){
-            audio = new Audio("drop_002.ogg");
-            audio.play()}
+    }
+  }
+  $(".col").attr("data-clicked","f")
+  $("#selectBar .col").click(function () {
+    id = $(this).index() + 1;
+    notbroken = true;
+    for (var i = 6; i > 1; i--) {
+      elem = $(".row:nth-child(" + i + ") .col:nth-child(" + id + ")");
+      if (elem.attr("data-clicked") == "f") {
+        elem.html(player ? "a" : "b");
+        elem.attr("data-clicked", (player ? "a":"b" ));
+        notbroken = false;
+        break;
+      }
+    }
+    if (!notbroken) {
+      if (audioEnabled) {
+        audio = new Audio("drop_002.ogg");
+        audio.play();
+      }
+      if (checkVictory()) {
+      } else {
         player = !player;
-      }//
-      else {
-          if(audioEnabled){
-        audio = new Audio("error_008.ogg");
-        audio.play()}
-
       }
-      ;
-    });
-
+    } //
+    else {
+      if (audioEnabled) {
+        audio = new Audio("error_008.ogg");
+        audio.play();
+      }
+    }
   });
+});
